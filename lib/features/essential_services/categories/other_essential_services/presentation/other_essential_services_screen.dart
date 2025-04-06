ca119732eca66f8f593c5b91d6c7ccb699e0a798
Trương Công Minh <32140397+truongcongminh96@users.noreply.github.com>
2025-04-06 13:28:10 +0700

    Refator animation OtherEssentialServicesScreen

--- lib/features/essential_services/categories/other_essential_services/presentation/other_essential_services_screen.dart
+++ lib/features/essential_services/categories/other_essential_services/presentation/other_essential_services_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sadec_smart_city/core/di/injector.dart';
-import 'package:sadec_smart_city/features/essential_services/categories/other_essential_services/data/models/other_essential_service_model.dart';
 import 'package:sadec_smart_city/features/essential_services/categories/other_essential_services/data/repositories/other_essential_services_repository.dart';
 import 'package:sadec_smart_city/features/essential_services/categories/other_essential_services/logic/other_essential_services_cubit.dart';
+import 'package:sadec_smart_city/features/essential_services/categories/other_essential_services/presentation/widgets/animated_service_item.dart';
 
 class OtherEssentialServicesScreen extends StatelessWidget {
   const OtherEssentialServicesScreen({super.key, required this.menuAppId});
@@ -49,7 +49,7 @@ class OtherEssentialServicesScreen extends StatelessWidget {
                     ),
                     itemBuilder: (context, index) {
                       final service = state.allOtherEssentialServices[index];
-                      return _AnimatedServiceItem(service: service);
+                      return AnimatedServiceItem(service: service);
                     },
                   ),
                 );
@@ -59,85 +59,6 @@ class OtherEssentialServicesScreen extends StatelessWidget {
               return const SizedBox.shrink();
             },
           ),
-    );
-  }
-}
-
-class _AnimatedServiceItem extends StatefulWidget {
-  final OtherEssentialServiceModel service;
-
-  const _AnimatedServiceItem({required this.service});
-
-  @override
-  State<_AnimatedServiceItem> createState() => _AnimatedServiceItemState();
-}
-
-class _AnimatedServiceItemState extends State<_AnimatedServiceItem> {
-  bool _pressed = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final isDark = theme.brightness == Brightness.dark;
-
-    return GestureDetector(
-      onTapDown: (_) => setState(() => _pressed = true),
-      onTapUp: (_) => setState(() => _pressed = false),
-      onTapCancel: () => setState(() => _pressed = false),
-      onTap: () {
-        // TODO: Handle tap
-      },
-      child: AnimatedScale(
-        scale: _pressed ? 0.97 : 1.0,
-        duration: const Duration(milliseconds: 150),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors:
-                  isDark
-                      ? [const Color(0xFF8439FF), const Color(0xFFEE58C5)]
-                      : [const Color(0xFFFFDEE9), const Color(0xFFFEC8D8)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withValues(alpha: 0.1),
-                blurRadius: 2,
-                offset: const Offset(0, 1),
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.network(
-                  widget.service.icon,
-                  width: 40,
-                  height: 40,
-                  fit: BoxFit.cover,
-                  errorBuilder:
-                      (_, __, ___) => const Icon(Icons.image, size: 24),
-                ),
-              ),
-              const SizedBox(width: 20),
-              Expanded(
-                child: Text(
-                  widget.service.ten,
-                  style: theme.textTheme.bodyMedium?.copyWith(
-                    color: isDark ? Colors.white : Colors.black,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              const Icon(Icons.chevron_right, size: 20),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
